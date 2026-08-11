37b26b3bc7798dfb7a9a17872e600ca2b270a1b8
Umar Kamani <umar.kamani@gmail.com>
2026-08-11 23:39:11 +0400

    First Commit

---
+++ src/main/java/mu/rekolt/Main.java
@@ -0,0 +1,60 @@
+package mu.rekolt;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        int choice = 0;
+
+
+        System.out.println("Welcome to The REKOLT Planters’ Cooperative Produce Tracker");
+        System.out.println("___________________________________________________________ \n");
+        System.out.println("Main Menu");
+        System.out.println("_________ \n");
+        System.out.println("""
+                1. Record a Delivery
+                2. Planter Menu
+                3. Delivery Menu
+                4. Reporting Center
+                5. Generate Season's Report
+                6. Exit
+                """);
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            try {
+                System.out.print("Enter Your Choice: ");
+                choice = scanner.nextInt();
+                if (choice < 1 || choice > 5) {
+                    System.out.println("Invalid Choice! Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice! Please try again using a number from 1 to 5.");
+                scanner.nextLine(); //clearing scanner buffer so that it doesn't result in an infinite loop
+            }
+        }
+        while (choice < 1 || choice > 5);
+
+        switch (choice) {
+            case 1:
+                System.out.println("Choice 1");
+                break;
+            case 2:
+                System.out.println("Choice 2");
+                break;
+            case 3:
+                System.out.println("Choice 3");
+                break;
+            case 4:
+                System.out.println("Choice 4");
+                break;
+            default:
+                System.out.println("Choice 5");
+                break;
+
+        }
+
+    }
+}
