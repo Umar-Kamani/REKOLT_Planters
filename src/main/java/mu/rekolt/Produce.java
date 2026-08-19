510912c6e179c2b13e86ea621b686be16ccd706f
Umar Kamani <umar.kamani@gmail.com>
2026-08-19 19:56:15 +0400

    Implemented abstract class Produce.java

--- src/main/java/mu/rekolt/Produce.java
+++ src/main/java/mu/rekolt/Produce.java
@@ -1,12 +1,13 @@
 package mu.rekolt;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Produce {
+public abstract class Produce {
     private final String name;
     private final int pricePerKg;
-    private final String category;
+
+    public Produce(String name, int pricePerKg) {
+        this.name = name;
+        this.pricePerKg = pricePerKg;
+    }
 
     public String getName() {
         return name;
@@ -16,20 +17,5 @@ public class Produce {
         return pricePerKg;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public Produce(String name, int pricePerKg, String category) {
-        this.name = name;
-        this.pricePerKg = pricePerKg;
-        this.category = category;
-    }
-
-    static HashMap<String, Produce> pricelist = new HashMap<>(Map.ofEntries(
-            Map.entry("MZE", new Produce("Maize",30,"Cereal")),
-            Map.entry("BNS", new Produce("Beans",90,"Cereal")),
-            Map.entry("POT", new Produce("Potatoes",45,"Perishable")),
-            Map.entry("TEA", new Produce("Green Tea Leaf",25,"Cash Crop"))
-            ));
+    public abstract double calculateValue();
 }
