9fa4aecfb2fd229623595da7b3f615b7a010c9bd
Umar Kamani <umar.kamani@gmail.com>
2026-08-19 15:02:09 +0400

    ID Generator

---
+++ src/main/java/mu/rekolt/IdGen.java
@@ -0,0 +1,13 @@
+package mu.rekolt;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class IdGen {
+ // Start the sequence at 1
+    private static final AtomicInteger sequence = new AtomicInteger(1);
+
+    public static int getNextId() {
+        // Increments the value by 1 and returns it atomically
+        return sequence.getAndIncrement();
+    }
+}
+
