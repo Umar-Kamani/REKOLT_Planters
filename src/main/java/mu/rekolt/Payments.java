d60d1b3a4e3acfa07445f5d87971720469c0a5f1
Umar Kamani <umar.kamani@gmail.com>
2026-08-21 18:00:50 +0400

    Finished implementation of payout calculator

--- src/main/java/mu/rekolt/Payments.java
+++ src/main/java/mu/rekolt/Payments.java
@@ -1,5 +1,4 @@
 package mu.rekolt;
-import mu.rekolt.Produce;
 import static mu.rekolt.Delivery.deliveries;
 
 
@@ -13,13 +12,11 @@ produce code
 produce quality
 mass
 
-the produce code will be used to lookup an enum, where we will store the produce prices as constants
+the produce code will be used to look up an enum, where we will store the produce prices as constants
 
 the produce quality will be passed into another function that will classify the quality of the produce on a
 grade basis (ABC & Reject) the function will return ABC and Reject. if reject then we will skip all calculation and return
-zero values. ABC will lookup in an enum to get their multiplier. The multiplier will be stored in a variable
-
-
+zero values. ABC will look up in an enum to get their multiplier. The multiplier will be stored in a variable
 
  */
 
@@ -58,25 +55,73 @@ public class Payments {
         if (produce_quality_score >= 85) {
             return Grade.A;
 
-        } else if (produce_quality_score >= 50 && produce_quality_score <= 84) {
+        } else if (produce_quality_score >= 70 && produce_quality_score < 85) {
             return Grade.B;
 
-        } else if (produce_quality_score >= 50 && produce_quality_score <= 69) {
+        } else if (produce_quality_score >= 50 && produce_quality_score < 70) {
             return Grade.C;
         } else {
             return Grade.REJECT;
         }
     }
 
-    public static void paymentCalculator(int delivery_id, String produce_code, double produce_mass, int produce_quality_score) {
+    public static void paymentCalculator(String delivery_id) {
+
+        int produce_quality_score = 0;
+        String produce_code = "";
+        int produce_mass = 0;
+        double grade_multiplier;
+        double category_multiplier = 0.0;
+        double commission_rate = 0.0;
+        int transport_levy = 2;
+        int produce_price;
+        Delivery delivery_obj = null;
+
+        double base_value;
+        double graded_multiplier_value;
+        double category_multiplier_value;
+        double transport_levied_value = 0.0;
+        double commission_value = 0.0;
+        double net_payable_value = 0.0;
+
+        /* calculation workflow
+        * 1. Base_value = produce_price * produce_mass
+        * 2. graded_multiplier_value = base_value * grade multiplier
+        * 3. category_multiplier_value = graded_multipllier_value * category_multiplier
+        * 4. transport_levied_value = transport_levy * produce_mass
+        * 5. commission_value = commission_rate * category_multiplier_value
+        * 6. net_payable_value = category_multiplier_value - commission_value - transport_levied_value */
+
+        for  (Delivery delivery : deliveries) {
+            if (delivery.delivery_id.equals(delivery_id)) {
+                produce_quality_score = delivery.produce_quality_score;
+                produce_code = delivery.produce_code;
+                produce_mass = delivery.produce_mass;
+                delivery = delivery_obj;
+            }
+        }
+
+        produce_price = ProduceDatabase.priceList.get(produce_code).getPricePerKg();
+        category_multiplier = CategorySelector.CatSelector(produce_code);
 
 
-        
+        base_value = produce_mass * produce_price;
+
+        //line 107 - 112 fetches the grade multiplier from the enum according to the specific delivery object
         Enum<Grade> grade = gradeClassifier(produce_quality_score);
 //        System.out.println("Grade: " + grade);
         Grade produce_multiplier = Grade.valueOf(grade.name());
-        double multiplier = produce_multiplier.getMultiplier();
-//        System.out.println("Multiplier: " + produce_multiplier);
+        grade_multiplier = produce_multiplier.getMultiplier();
+//        System.out.println("Multiplier: " + grade_multiplier);
+        graded_multiplier_value = base_value * grade_multiplier;
+
+        category_multiplier_value = category_multiplier * graded_multiplier_value;
+
+        transport_levied_value = transport_levy * produce_mass;
+
+        commission_value = commission_rate * category_multiplier_value;
+
+        net_payable_value = category_multiplier_value - (transport_levied_value + commission_value);
 
     }
 
