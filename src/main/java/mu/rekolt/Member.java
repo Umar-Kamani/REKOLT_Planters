7ac1aca6b69eb53238c4c85b4f4e2a3784f4e7fa
Umar Kamani <umar.kamani@gmail.com>
2026-08-23 13:45:00 +0400

    Created Member class

---
+++ src/main/java/mu/rekolt/Member.java
@@ -0,0 +1,11 @@
+package mu.rekolt;
+
+public class Member {
+    String member_code;
+    String member_name;
+
+    public Member(String member_code, String member_name) {
+        this.member_code = member_code;
+        this.member_name = member_name;
+    }
+}
