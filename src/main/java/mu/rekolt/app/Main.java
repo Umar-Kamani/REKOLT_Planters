b1123af775d2d1fde69670607e88263e2a088ff9
Umar Kamani <umar.kamani@gmail.com>
2026-08-28 18:59:25 +0400

    Added SampleData to load sample data into the program

--- src/main/java/mu/rekolt/app/Main.java
+++ src/main/java/mu/rekolt/app/Main.java
@@ -4,8 +4,10 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import mu.rekolt.model.Delivery;
+import mu.rekolt.service.DocumentGenerator;
 import mu.rekolt.service.Payments;
 import mu.rekolt.model.ProduceDatabase;
+import mu.rekolt.service.SampleData;
 import mu.rekolt.service.SeasonReporting;
 
 public class Main {
@@ -15,7 +17,7 @@ public class Main {
 
     do {
         System.out.println();
-        System.out.println("Welcome to The REKOLT Planters’ Cooperative Produce Tracker");
+        System.out.println("Welcome to The REKOLT Planter's Cooperative Produce Tracker");
         System.out.println("___________________________________________________________ \n");
         System.out.println("Main Menu");
         System.out.println("_________ \n");
@@ -23,7 +25,8 @@ public class Main {
                     1. Record a Delivery
                     2. Season Figures on Screen
                     3. Generate Season Report
-                    4. Exit
+                    4. Load Sample Data
+                    5. Exit
                     """);
 
         Scanner scanner = new Scanner(System.in);
@@ -31,7 +34,7 @@ public class Main {
             try {
                 System.out.print("Enter Your Choice: ");
                 choice = scanner.nextInt();
-                if (choice < 1 || choice > 4) {
+                if (choice < 1 || choice > 5) {
                     System.out.println("Invalid Choice! Please try again.");
                 }
             } catch (InputMismatchException e) {
@@ -39,7 +42,7 @@ public class Main {
                 scanner.nextLine(); //clearing scanner buffer so that it doesn't result in an infinite loop
             }
         }
-        while (choice < 1 || choice > 4);
+        while (choice < 1 || choice >5);
 
 
         switch (choice) {
@@ -56,18 +59,23 @@ public class Main {
                 SeasonReporting.displayTopDeliveries(5);
                 break;
             case 3:
-                System.out.println("Choice 3");
+                DocumentGenerator.generateSeasonReport();
                 break;
             case 4:
-                System.out.println("Choice 4");
+                System.out.println("Loading Sample Data...");
+                System.out.println("\n\n");
+                SampleData.loadSampleDeliveries();
+                break;
+            case 5:
+                System.out.println("Goodbye!!!");
+                System.exit(0);
                 break;
             default:
-                System.out.println("Choice 5");
                 break;
 
         }
 
     }
-    while (choice !=4);
+    while (true);
     }
 }
